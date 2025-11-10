6b315621b1553e5b4400bbbcf3255d5d2dde96c0
Hiwonder-docs <net@hiwonder.com>
2025-11-10 19:13:00 +0800

    Color Sensor 2

--- source/conf.py
+++ source/conf.py
@@ -119,6 +119,10 @@ html_static_path = ['_static']
 html_css_files = ['style.css']
 html_js_files = ['custom.js']
 html_theme_options = {
+    'logo': {
+        'text': 'Home',
+        'link': 'https://wiki.hiwonder.com/',  # 主项目URL
+    },
     'style_nav_header_background': '#f98800',
     'logo_only': True,
     'logo_link': 'https://your-main-project.readthedocs.io/',
