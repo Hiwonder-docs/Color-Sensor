e746b9878d0aec0bccce0d51078b4d3c28111ef3
Hiwonder-docs <net@hiwonder.com>
2025-11-10 10:45:02 +0800

    color sensor 3

--- source/conf.py
+++ source/conf.py
@@ -112,8 +112,8 @@ myst_enable_extensions = [
     "dollarmath",
 ]
 
-html_url='';
-html_logo = '_static/logo.png';
+html_url='https://wiki.hiwonder.com/'
+html_logo = '_static/logo.png'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = ['style.css']
